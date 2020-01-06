26c437b409c7df9ef462f897f7c03ab2ff73c126
Stuart Henderson <kryten68@googlemail.com>
2020-01-06 20:50:18 +0000

    Cleaned up slice handler

--- easyslice/easyslice.go
+++ easyslice/easyslice.go
@@ -1,25 +1,12 @@
 package easyslice
 
-import (
-	"fmt"
-)
-
 func RemoveStringFromSlice(slc []string, s string) []string {
-
 	newSlice := make([]string, 0)
-
-	fmt.Println("Received: ")
-	for _, v := range slc {
-		fmt.Println(v)
-	}
-
 	for _, v := range slc {
 		if v != s {
 			newSlice = append(newSlice, v)
 		}
 	}
-
 	slc = nil
 	return newSlice
-
 }
